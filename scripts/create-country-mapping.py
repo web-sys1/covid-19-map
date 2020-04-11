fa2d76560242d88242568a9453534733bb2f5a13
Web Source (SRC) <61905565+web-sys1@users.noreply.github.com>
2020-04-11 15:24:31 +0300

    Update create-country-mapping.py

--- scripts/create-country-mapping.py
+++ scripts/create-country-mapping.py
@@ -4,7 +4,7 @@ import csv
 import json
 import pycountry
 
-confirmed_data_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+confirmed_data_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 
 try:
   response = urllib.request.urlopen(confirmed_data_url)
